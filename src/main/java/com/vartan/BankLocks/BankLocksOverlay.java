b2cc6aa3eac84751834397ec6cb1ea694fba2028
Michael Vartan <michael.wayne.vartan@gmail.com>
2024-11-28 08:49:00 -0800

    Minor optimization for overlay; exit early on cheap operations.

--- src/main/java/com/vartan/BankLocks/BankLocksOverlay.java
+++ src/main/java/com/vartan/BankLocks/BankLocksOverlay.java
@@ -23,22 +23,22 @@ public class BankLocksOverlay extends WidgetItemOverlay {
 
     @Override
     public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem) {
-        graphics.setFont(FontManager.getRunescapeSmallFont());
-
-        if (!plugin.getLockedItemIds().contains(itemId)) {
+        // Clamp opacity between 0-1.
+        float opacity = (float) Math.max(0, Math.min(1, config.lockOpacity()));
+        if (opacity <= 0f || !plugin.getLockedItemIds().contains(itemId) || plugin.lockImage == null) {
             return;
         }
+
         Rectangle location = widgetItem.getCanvasBounds();
         int width = (int) location.getWidth();
         int height = (int) location.getHeight();
         int x = (int) location.getX();
         int y = (int) location.getY();
-        float opacity = (float) Math.max(0, Math.min(1, config.lockOpacity()));
-        if (plugin.lockImage != null && opacity > 0f) {
-            // Draw transparent lock over the item.
-            AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
-            graphics.setComposite(composite);
-            graphics.drawImage(plugin.lockImage, x + width / 2, y + height / 2, width / 2, height / 2, null);
-        }
+
+        // Draw transparent lock over the item.
+        graphics.setFont(FontManager.getRunescapeSmallFont());
+        AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
+        graphics.setComposite(composite);
+        graphics.drawImage(plugin.lockImage, x + width / 2, y + height / 2, width / 2, height / 2, null);
     }
 }
