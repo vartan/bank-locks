a272e86e76462baaa016b15878b0eac757730a77
azeroverdose <Ezivoz@gmail.com>
2025-12-16 22:07:42 +0100

    Use Collectors.toCollection(HashSet::new) instead, with no saved config set, it returns an immutable set, which can't be modified in SetUtil.java:11

--- src/main/java/com/vartan/BankLocks/BankLocksLoader.java
+++ src/main/java/com/vartan/BankLocks/BankLocksLoader.java
@@ -4,6 +4,7 @@ import net.runelite.client.config.ConfigManager;
 
 import javax.inject.Inject;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,17 +26,17 @@ public class BankLocksLoader {
     /** Returns the set of item IDs saved to config, or an empty set if it doesn't exist. */
     public Set<Integer> loadLockedItems() {
         try {
-        String commaSeparatedItemIds = configManager.getConfiguration(BankLocksConfig.CONFIG_GROUP,
-                BankLocksConfig.LOCKED_ITEMS_CONFIG_NAME);
+            String commaSeparatedItemIds = configManager.getConfiguration(BankLocksConfig.CONFIG_GROUP,
+                    BankLocksConfig.LOCKED_ITEMS_CONFIG_NAME);
             if (commaSeparatedItemIds != null) {
                 return Arrays.stream(commaSeparatedItemIds.split(","))
                         .map(Integer::parseInt)
-                        .collect(Collectors.toSet());
+                        .collect(Collectors.toCollection(HashSet::new));
             }
         } catch (Exception e) {
             // If there is any exception encountered (reading config or converting to Set<Integer>),
             // behave as if there is no saved config.
         }
-        return Set.of();
+        return new HashSet<>();
     }
 }
